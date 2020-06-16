08c6cb66a0ed6392c9058d15e48935cd71b10b3d
Arilson Santos <arilson@msn.com>
2020-06-15 23:42:23 -0300

    Initial commit

---
+++ bank/src/test/java/br/com/orion/bank/PaymentControllerNativeTest.java
@@ -0,0 +1,9 @@
+package br.com.orion.bank;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class PaymentControllerNativeTest extends PaymentControllerTest {
+
+    // Execute the same tests but in native mode.
+}
